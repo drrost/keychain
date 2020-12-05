aa46e0f81bd7fc9793465ae8600dd17cf3ed1a6c
Rostyslav Druzhchenko <rdruzhchenko@teamworkcommerce.com>
2020-12-05 22:52:46 +0200

    Add versions to the Package.swfit file

--- Package.swift
+++ Package.swift
@@ -4,6 +4,10 @@ import PackageDescription
 
 let package = Package(
     name: "Keychain",
+    platforms: [
+        .macOS(.v10_15),
+        .iOS(.v13)
+    ],
     products: [
         .library(
             name: "Keychain",
